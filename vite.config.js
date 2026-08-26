6e5948ff5c54e353fdd0033d0e315e6143a9e8f3
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-08-26 04:44:04 +0300

    edit in index.css

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 import flowbiteReact from "flowbite-react/plugin/vite";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-      tailwindcss(),
-    flowbiteReact()
+    tailwindcss(),
+    flowbiteReact(),
   ],
-})
+});
